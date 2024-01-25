0066b9e24d2a9fc5032c352ee610e72174294a4f
peacemaker474 <hc456123@gmail.com>
2024-01-25 22:58:59 +0900

    chore: ESLint import order 일부 적용 및 playwright 옵션 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import '../styles/common.scss';
 
-import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 
