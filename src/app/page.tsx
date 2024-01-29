de118b5491102842a7e101ec21fe3438e13052f6
peacemaker474 <hc456123@gmail.com>
2024-01-29 22:23:32 +0900

    styles: scss 변수 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,22 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-import styles from './root.module.scss';
+import styles from './main.module.scss';
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.main__container}>
+      <div className={styles.container}>
         <Image src="/assets/icons/mainLogo.svg" width={380} height={380} alt="메인로고_이미지" />
       </div>
-      <div className={`${styles.main__container} ${styles.main__content} `}>
-        <h1 className={styles.main__title}> 지금 일어나고 있는 일 </h1>
-        <h2 className={styles.main__subTitle}> 지금 가입하세요. </h2>
-        <Link href="/i/flow/signup" className={styles.main__signupLink}>
+      <div className={`${styles.container} ${styles.content} `}>
+        <h1 className={styles.title}> 지금 일어나고 있는 일 </h1>
+        <h2 className={styles.subTitle}> 지금 가입하세요. </h2>
+        <Link href="/i/flow/signup" className={styles.signupLink}>
           계정 생성
         </Link>
-        <span className={styles.main__loginText}> 이미 계정이 있으신가요? </span>
-        <Link href="/login" className={`${styles.main__signupLink} ${styles.main__loginLink}`}>
+        <span className={styles.loginText}> 이미 계정이 있으신가요? </span>
+        <Link href="/login" className={`${styles.signupLink} ${styles.loginLink}`}>
           로그인
         </Link>
       </div>
