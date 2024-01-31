e0128baf64664f7b9783052844f4c26b0ae047b9
peacemaker474 <hc456123@gmail.com>
2024-01-31 22:08:57 +0900

    refactor: /login 에서 /i/flow/login으로 리다이렉트 수정

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,5 +1,31 @@
-import { redirect } from 'next/navigation';
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/navigation';
+
+import styles from '@/styles/main/main.module.scss';
 
 export default function RedirectLoginPage() {
-  redirect('/i/flow/login');
+  const router = useRouter();
+  router.replace('/i/flow/login');
+
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <Image src="/assets/icons/mainLogo.svg" width={380} height={380} alt="메인로고_이미지" />
+      </div>
+      <div className={`${styles.container} ${styles.content} `}>
+        <h1 className={styles.title}> 지금 일어나고 있는 일 </h1>
+        <h2 className={styles.subTitle}> 지금 가입하세요. </h2>
+        <Link href="/i/flow/signup" className={styles.signupLink}>
+          계정 생성
+        </Link>
+        <span className={styles.loginText}> 이미 계정이 있으신가요? </span>
+        <Link href="/login" className={`${styles.signupLink} ${styles.loginLink}`}>
+          로그인
+        </Link>
+      </div>
+    </main>
+  );
 }
