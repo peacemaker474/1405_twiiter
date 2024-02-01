4b4714f743898abe62e23134c71b6dc0f4dee5a4
peacemaker474 <hc456123@gmail.com>
2024-02-01 20:50:46 +0900

    refactor: 기존 Public에서 사용하던 레이아웃 컴포넌트로 분리

---
+++ src/app/(public)/_component/PublicLayout.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from '@/styles/main/main.module.scss';
+
+export default function PublicLayout() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <Image src="/assets/icons/mainLogo.svg" width={380} height={380} alt="메인로고_이미지" />
+      </div>
+      <div className={`${styles.container} ${styles.content} `}>
+        <h1 className={styles.title}> 지금 일어나고 있는 일 </h1>
+        <h2 className={styles.subTitle}> 지금 가입하세요. </h2>
+        <Link href="/i/flow/signup" className={styles.signupLink}>
+          계정 생성
+        </Link>
+        <span className={styles.loginText}> 이미 계정이 있으신가요? </span>
+        <Link href="/login" className={`${styles.signupLink} ${styles.loginLink}`} aria-label="login-link">
+          로그인
+        </Link>
+      </div>
+    </main>
+  );
+}
