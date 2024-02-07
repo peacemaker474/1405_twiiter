b618067cc58198a8f4cf4fbd57832b763bf87668
peacemaker474 <hc456123@gmail.com>
2024-02-07 22:46:18 +0900

    style: 로그인 후 레이아웃 추가

--- src/app/(private)/layout.tsx
+++ src/app/(private)/layout.tsx
@@ -1,7 +1,37 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import styles from '@/styles/main/privateMain.module.scss';
+
 export default function PrivateLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main> {children} </main>;
+  return (
+    <div className={styles.container}>
+      <header className={styles.leftContainer}>
+        <nav className={styles.leftWrapper}>
+          <ul className={styles.globalMenu}>
+            <Link href="/home" className={styles.logoLink}>
+              <div className={styles.logoLinkWrapper}>
+                <Image src="/assets/icons/mainLogo.svg" width={25} height={25} alt="메인로고" />
+              </div>
+            </Link>
+          </ul>
+        </nav>
+      </header>
+      <main className={styles.rightContainer}>
+        <section className={styles.rightWrapper}>
+          <div className={styles.mainContent}>{children}</div>
+          <div className={styles.rightContent}>
+            <form className={styles.search}>
+              <Image src="/assets/icons/search.svg" width={20} height={20} alt="검색_아이콘" />
+              <input type="search" className={styles.searchInput} />
+            </form>
+          </div>
+        </section>
+      </main>
+    </div>
+  );
 }
